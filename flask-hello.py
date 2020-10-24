c0d976871005617ca3cc97ae0572f3862e025e11
Anton Razumov <razumov@gmail.com>
2020-10-24 15:30:38 -0400

    v00

---
+++ flask-hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+load_dotenv('.flaskenv')
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<user>')
+@app.route('/hello')
+@app.route('/hello/<user>')
+def hello_world(user=None):
+    user = user or '--'
+    return render_template('index.html', user=user)
+
+if __name__ == '__main__':
+    app.run()
+    # app.run(host='0.0.0.0', port=80)
+
